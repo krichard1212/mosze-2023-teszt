cd78fc7ce18274d517cb42b5c2ab0e4c457b88c2
krichard12 <richardkohegyi@gmail.com>
2023-09-24 13:43:08 +0200

    hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // hiányzik a "_"
+    std::cout << '1-100 ertekek duplazasa' // endl szükséges formázás miatt, ; hiányzik
+    for (int i = 0;) // hiányzik az az i++, illetve a feltétel, i=1
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // i--> "i<=N_ELEMENTS"-re változtatása szükséges, i=1
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzik a változó(b[i]) kiírása illetve az endl formázás miatt, ; 
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
+    std::cout << "Atlag szamitasa: " << std::endl;  
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett ; illetve a feltételnél meg kell engedni az egyenlőséget is
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
