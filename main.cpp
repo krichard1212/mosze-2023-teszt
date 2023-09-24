b7accd7b894cad75efb2939bcf7f16efe6ca6db0
krichard12 <richardkohegyi@gmail.com>
2023-09-24 13:44:18 +0200

    kommentek törlése és hibák javítása

--- main.cpp
+++ main.cpp
@@ -2,25 +2,21 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS]; // hiányzik a "_"
-    std::cout << '1-100 ertekek duplazasa' // endl szükséges formázás miatt, ; hiányzik
-    for (int i = 0;) // hiányzik az az i++, illetve a feltétel, i=1
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++) // i--> "i<=N_ELEMENTS"-re változtatása szükséges, i=1
-    {
-        std::cout << "Ertek:" //hiányzik a változó(b[i]) kiírása illetve az endl formázás miatt, ; 
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;  
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett ; illetve a feltételnél meg kell engedni az egyenlőséget is
-    {
-        atlag += b[i] // ; hiányzik
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+int main() {
+  int *b = new int[N_ELEMENTS];
+  std::cout << '1-100 ertekek duplazasa' << std::endl;
+  for (int i = 1; i <= N_ELEMENTS; i++) {
+    b[i] = i * 2;
+  }
+  for (int i = 1; i <= N_ELEMENTS; i++) {
+    std::cout << "Ertek:" << b[i] << std::endl;
+  }
+  std::cout << "Atlag szamitasa: " << std::endl;
+  int atlag;
+  for (int i = 0; i <= N_ELEMENTS; i++) {
+    atlag += b[i];
+  }
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  return 0;
 }
