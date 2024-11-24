5747907a7410ff2e707f9175464febfae5b0f40c
krichard1212 <richardkohegyi@gmail.com>
2024-11-24 22:56:13 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -4,23 +4,30 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];   // N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'  // hiányzika
-    for (int i = 0;)   // Hiányzikaz i++
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)    // végtelen loop
-    {
-        std::cout << "Ertek:"  // érték hiáynzik, ;
-    }
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;    // inicializálás hiáynzik
-    for (int i = 0; i < N_ELEMENTS, i++)   // ;
-    {
-        atlag += b[i]   %% ;
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+   int *b = new int[N_ELEMENTS];
+   std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+   for (int i = 0; i < N_ELEMENTS; i++)
+   {
+       b[i] = i * 2;
+   }
+
+   for (int i = 0; i < N_ELEMENTS; i++)
+   {
+       std::cout << "Ertek: " << b[i] << std::endl;
+   }
+
+   std::cout << "Atlag szamitasa: " << std::endl;
+   int atlag = 0;
+
+   for (int i = 0; i < N_ELEMENTS; i++)
+   {
+       atlag += b[i];
+   }
+
+   atlag /= N_ELEMENTS;
+   std::cout << "Atlag: " << atlag << std::endl;
+
+   delete[] b;
+   return 0;
 }
