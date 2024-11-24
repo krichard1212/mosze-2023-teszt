0f5a8fcc5e4ce824d8955528e7ca342ffd4acbb4
krichard1212 <richardkohegyi@gmail.com>
2024-11-24 22:55:29 +0100

    comment

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];   // N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  // hiányzika
+    for (int i = 0;)   // Hiányzikaz i++
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)    // végtelen loop
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:"  // érték hiáynzik, ;
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;    // inicializálás hiáynzik
+    for (int i = 0; i < N_ELEMENTS, i++)   // ;
     {
-        atlag += b[i]
+        atlag += b[i]   %% ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
